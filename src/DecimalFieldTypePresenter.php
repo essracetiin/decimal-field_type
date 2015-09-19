dcb3f65367fcc9c6225d1a100c0700b0a285c5da
Ryan Thompson <ryan@pyrocms.com>
2015-09-19 04:45:23 -0500

    Currency

--- src/DecimalFieldTypePresenter.php
+++ src/DecimalFieldTypePresenter.php
@@ -1,6 +1,7 @@
 <?php namespace Anomaly\DecimalFieldType;
 
 use Anomaly\Streams\Platform\Addon\FieldType\FieldTypePresenter;
+use NumberFormatter;
 
 /**
  * Class DecimalFieldTypePresenter
@@ -25,5 +26,25 @@ class DecimalFieldTypePresenter extends FieldTypePresenter
         $point     = array_get($this->object->getConfig(), 'point', '.');
 
         return number_format($this->object->getValue(), $decimals, $point, str_replace('&#160;', ' ', $separator));
+    }
+
+    /**
+     * Return the integer formatted as a currency.
+     *
+     * @param null   $currency
+     * @param string $field
+     * @return string
+     */
+    public function currency($currency = null, $field = 'currency')
+    {
+        if (!$currency) {
+            $currency = $this->object->getEntry()->{$field};
+        }
+
+        $format = new NumberFormatter(app('app.locale') . "@currency={$currency}", NumberFormatter::CURRENCY);
+
+        $symbol = $format->getSymbol(NumberFormatter::CURRENCY_SYMBOL);
+
+        return $symbol . $this->formatted();
     }
 }
