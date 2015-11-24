47b0ef1b20aaf657923a49da7b48d6aee95781d4
Ryan Thompson <ryan@pyrocms.com>
2015-11-24 00:46:02 -0600

    new init

--- resources/js/input.js
+++ resources/js/input.js
@@ -1,7 +1,7 @@
 $(function () {
 
     // Initialize decimals
-    $('.decimal-field-type input').each(function () {
+    $('.decimal-field_type input').each(function () {
         $(this).spinner({
             min: $(this).data('min'),
             max: $(this).data('max'),
